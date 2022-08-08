612affcc7d8aeb0da635f9956bf232d0a991af06
rajeshpal652 <rajesh.pal.652@.com>
2022-08-08 15:24:38 +0530

    Added: UC2

--- employeeWage.py
+++ employeeWage.py
@@ -1,7 +1,12 @@
 import random
 
+wage_per_hour = 20
+full_day_hour = 8
+
 empCheck = random.randint(0,1)
 if(empCheck == 0):
     print("Employee is Absent")
+    print("Daily Employee Wage : ", 0)
 else:
     print("Employee is Present")
+    print("Daily Employee Wage : ", wage_per_hour * full_day_hour)
