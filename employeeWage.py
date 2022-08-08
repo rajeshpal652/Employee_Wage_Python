bd810d6ebfa34cf1c78d9727cc2a564c1dfbb3ce
rajeshpal652 <rajesh.pal.652@.com>
2022-08-08 21:35:18 +0530

    Added: UC5 employee wage computation

--- employeeWage.py
+++ employeeWage.py
@@ -1,20 +1,30 @@
 import random
 
 wage_per_hour = 20
-full_day_hour = 8
-part_time_hour = 4
+days_per_month = 20
 
 is_absent = 0
 is_present = 1
 is_part_time = 2
 
-empCheck = random.randint(0,2)
-if(empCheck == is_absent):
-    print("Employee is Absent")
-    print("Daily Employee Wage : ", 0)
-elif(empCheck == is_present):
-    print("Employee is Present")
-    print("Daily Employee Wage : ", wage_per_hour * full_day_hour)
-else:
-    print("Employee is working Part Time")
-    print("Daily Employee Wage : ", wage_per_hour * part_time_hour)
+
+dayCount = 1
+empHrs = 0
+totalEmpWage = 0
+empWage = 0
+
+while (dayCount <= days_per_month):
+    empCheck = random.randint(0, 2)
+    if (empCheck == is_absent):
+        empHrs = 0
+    elif (empCheck == is_present):
+        empHrs = 8
+    else:
+        empHrs = 4
+
+    dayCount = dayCount + 1
+    empWage = wage_per_hour * empHrs
+    totalEmpWage += empWage
+    print("Total Employee Wage of the day : ", totalEmpWage)
+
+print("Total employee wage of the month : ", totalEmpWage)
