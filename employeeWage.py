6d8254e165740fa8c4e9b105f717bb7520a3d42b
rajeshpal652 <rajesh.pal.652@.com>
2022-08-08 15:30:09 +0530

    Added: UC3

--- employeeWage.py
+++ employeeWage.py
@@ -2,11 +2,19 @@ import random
 
 wage_per_hour = 20
 full_day_hour = 8
+part_time_hour = 4
 
-empCheck = random.randint(0,1)
-if(empCheck == 0):
+is_absent = 0
+is_present = 1
+is_part_time = 2
+
+empCheck = random.randint(0,2)
+if(empCheck == is_absent):
     print("Employee is Absent")
     print("Daily Employee Wage : ", 0)
-else:
+elif(empCheck == is_present):
     print("Employee is Present")
     print("Daily Employee Wage : ", wage_per_hour * full_day_hour)
+else:
+    print("Employee is working Part Time")
+    print("Daily Employee Wage : ", wage_per_hour * part_time_hour)
