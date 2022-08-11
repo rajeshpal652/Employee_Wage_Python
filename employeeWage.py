2390751a77c1b4264879c4b354f8140f4fb11e14
rajeshpal652 <rajesh.pal.652@.com>
2022-08-11 16:14:48 +0530

    Added: Refactored

--- employeeWage.py
+++ employeeWage.py
@@ -1,33 +1,37 @@
 import random
 
-wage_per_hour = 20
-days_per_month = 20
-max_total_working_hrs = 100
+class EmployeeWage():
+    wage_per_hour = 20
+    days_per_month = 20
+    max_total_working_hrs = 100
 
-is_absent = 0
-is_present = 1
-is_part_time = 2
+    is_absent = 0
+    is_present = 1
+    is_part_time = 2
 
+    dayCount = 1
+    empHrs = 0
+    totalEmpWage = 0
+    empWage = 0
+    totalEmpHrs = 0
 
-dayCount = 1
-empHrs = 0
-totalEmpWage = 0
-empWage = 0
-totalEmpHrs = 0
+    def wageCount(self):
+        while self.dayCount <= self.days_per_month and self.totalEmpHrs <= self.max_total_working_hrs:
+            empCheck = random.randint(0, 2)
+            if (empCheck == self.is_absent):
+                self.empHrs = 0
+            elif (empCheck == self.is_present):
+                self.empHrs = 8
+            else:
+                self.empHrs = 4
 
-while (dayCount <= days_per_month | totalEmpHrs <= max_total_working_hrs):
-    empCheck = random.randint(0, 2)
-    if (empCheck == is_absent):
-        empHrs = 0
-    elif (empCheck == is_present):
-        empHrs = 8
-    else:
-        empHrs = 4
+            self.dayCount = self.dayCount + 1
+            self.empWage = self.wage_per_hour * self.empHrs
+            self.totalEmpWage += self.empWage
+            self.totalEmpHrs += self.empHrs
+            print("Total Employee Wage of the day : ", self.totalEmpWage, " Day No : ", self.dayCount - 1)
 
-    dayCount = dayCount + 1
-    empWage = wage_per_hour * empHrs
-    totalEmpWage += empWage
-    totalEmpHrs += empHrs
-    print("Total Employee Wage of the day : ", totalEmpWage)
+        print("Total employee wage of the month : ", self.totalEmpWage)
 
-print("Total employee wage of the month : ", totalEmpWage)
+empWage = EmployeeWage()
+empWage.wageCount()
